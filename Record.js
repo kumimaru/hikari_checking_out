53af73881f9c875275380035c99c5ef81aeef3aa
Hisakata Matsumoto <kumimaru11m.h@gmail.com>
2020-07-07 06:53:11 +0900

    1.コミットしないファイルを指定 2.保育園入退室処理を実装

--- Record.js
+++ Record.js
@@ -1,84 +1,96 @@
 // メールから取得した入退室記録をカレンダーに追記
 function checking_out() {
-  var p_sheet = SpreadsheetApp.getActiveSheet();
+  // TODO: メールを取得する処理を実装する
+  // メールの送信者：奈良すこやか保育園 未読
+  const query = 'from:cdm7019rqBk@codmon.com is:unread';
+  // メールの検索範囲
+  const min = 0;
+  const max = 100;
 
-  // 未読の入退室メールを取得
-  var p_lastRow = p_sheet.getLastRow();
-  // 受信日時を取得
-  p_sheet.getRange(p_lastRow, 1).setValue(new Date());
-  p_sheet.getRange(p_lastRow, 1).setNumberFormat("yyyy/mm/dd HH:mm:ss")
+  // メールを検索
+  const threads = GmailApp.search(query, min, max);
+  const messagesForThreads = GmailApp.getMessagesForThreads(threads);
 
-  // 取得したメールの件名か入室か退室のどちらであるかを判定
-  if(isWakeup(p_lastRow)) {
-    // 退室の場合、入室から退室までの時間をカレンダーに設定
-    createSleepDiary(p_lastRow, p_lastRow - 1);
+  // 取得したメールから日時と件名を取得
+  const values = [];
+  for (const messages of messagesForThreads) {
+    const message = messages[0];
+    const record = [
+      message.getDate()
+      , message.getSubject()
+    ];
+    values.push(record);
+  }
+  if (values.length < 0) {
+    // メールが取得できなければ処理終了
     return;
   }
 
-  // 入室のカレンダーを作成
-  // カレンダー名を取得
-  var calendar_nm = p_sheet.getRange(p_lastRow, 2).getValue();  // すやあを取得
-  // 睡眠時間を取得
-  var p_bed_time = p_sheet.getRange(p_lastRow, 1).getValue();
-  Logger.log("すやあの時間:" + p_bed_time);
-  createEvent(calendar_nm, p_bed_time, p_bed_time, null);
+  // シートのフォーマットを指定の日時に設定
+  var lastRow = SpreadsheetApp.getActiveSheet().getLastRow();
+  sheet.getRange(lastRow, 1).setNumberFormat("yyyy/mm/dd HH:mm:ss")
+  // シートにメールの取得日時と件名を記録
+  SpreadsheetApp.getActiveSheet().getRange(lastRow, 1, values.length, values[0].length).setValues(values);
+
+  // 取得したメールの件名が入室か退室のどちらであるかを判定
+  if(values[1] === "【奈良すこやか保育園】入室のお知らせ") {
+    // 入室記録を取得
+    var checking_out = sheet.getRange(lastRow, 1).getValue();
+    // 入室のカレンダー名を取得
+    var calendar_nm = sheet.getRange(lastRow, 2).getValue();
+    // カレンダーを作成
+    createEvent(calendar_nm, checking_out, checking_out, null);
+    return;
+  }
+
+  // 退室の場合、入室から退室までの時間をカレンダーに設定
+  createTimeCheckingOut(lastRow - 1, lastRow);
 }
 
-// 行の値が光ちゃんおはであるか判定
-function isWakeup(x_row) {
-  var p_sheet = SpreadsheetApp.getActiveSheet();
-  Logger.log("行の値:" + p_sheet.getRange(x_row, 2).getValue());
-  return p_sheet.getRange(x_row, 2).getValue() == "睡眠"
-}
-
-// 睡眠履歴をカレンダーに作成する
-function createSleepDiary(x_wakeup_row, x_sleep_row) {
-  var p_sheet = SpreadsheetApp.getActiveSheet();
-
-  var p_bed_time = p_sheet.getRange(x_sleep_row, 1).getValue();
-  var p_wakeup_time = p_sheet.getRange(x_wakeup_row, 1).getValue();
-  var p_sleeping_time = getDiff(p_bed_time, p_wakeup_time);
-  Logger.log("睡眠時間:" + p_sleeping_time);
+// 入退室記録をカレンダーに作成する
+function createTimeCheckingOut(x_first_row, x_last_row) {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var enter_time = sheet.getRange(x_first_row, 1).getValue();
+  var checking_out = sheet.getRange(x_last_row, 1).getValue();
+  var checking_out_time = getDiff(checking_out, enter_time);
+  Logger.log("入退室:" + checking_out_time);
 
   // カレンダーを作成する
-  var calendar_nm = p_sheet.getRange(x_wakeup_row, 2).getValue();  // 睡眠を取得
-  createEvent(calendar_nm, p_bed_time, p_wakeup_time, p_sleeping_time);
+  var calendar_nm = sheet.getRange(x_last_row, 2).getValue();
+  createEvent(calendar_nm, checking_out, enter_time, checking_out_time);
 }
 
 // カレンダーに日付をセットする
-function createEvent(x_calendar_nm, x_bed_time, x_wakeup_time, x_sleeping_time){
+function createEvent(x_calendar_nm, x_last_time, x_enter_time, x_sleeping_time){
   Logger.log("カレンダー名:" + x_calendar_nm);
 
   // 光ちゃんカレンダー
   var hikari_calendar = PropertiesService.getScriptProperties().getProperty("HIKARI_CALENDAR");
   var calendar = CalendarApp.getCalendarById(hikari_calendar);
-  calendar.createEvent(x_calendar_nm, new Date(x_bed_time), new Date(x_wakeup_time) , {description: x_sleeping_time});
+  calendar.createEvent(x_calendar_nm, new Date(x_last_time), new Date(x_enter_time) , {description: x_sleeping_time});
 }
 
-// 睡眠時間を取得
-function getDiff(x_bed_time, x_wakeup_time) {
-  // テスト用
-//  x_bed_time = '2018/12/16 8:19:58'
-//  x_wakeup_time = '2018/12/16 11:07:20'
+// 差分の時間を取得
+function getDiff(x_last_time, x_enter_time) {
 
-  var p_bed_time = Moment.moment(x_bed_time);
-  var p_wakeup_time = Moment.moment(x_wakeup_time);
-  Logger.log("bed_time:" + p_bed_time);
-  Logger.log("wakeup_time:" + p_wakeup_time);
+  var checking_out = Moment.moment(x_last_time);
+  var enter_time = Moment.moment(x_enter_time);
+  Logger.log("checking_out:" + checking_out);
+  Logger.log("enter_time:" + enter_time);
 
   // 時間計算
-  var p_hour = p_wakeup_time.diff(p_bed_time,"h");
-  Logger.log("p_hour:" + p_hour);
+  var hour = enter_time.diff(checking_out,"h");
+  Logger.log("hour:" + hour);
 
   // 分計算
-  var p_minute = p_wakeup_time.diff(p_bed_time,"m");
-  Logger.log("p_minute:" + p_minute);
+  var minute = enter_time.diff(checking_out,"m");
+  Logger.log("minute:" + minute);
 
-  var p_mm = p_minute - (p_hour * 60);
-  Logger.log("分:" + p_mm);
+  var mm = minute - (hour * 60);
+  Logger.log("分:" + mm);
 
   // 結果
-  var p_result = p_hour + '時間' + p_mm + '分';
-  Logger.log('結果：' + p_result);
-  return p_result;
+  var result = hour + '時間' + mm + '分';
+  Logger.log('結果：' + result);
+  return result;
 }
